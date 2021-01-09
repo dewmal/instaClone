61786a4c1862a3b069502309d4ee1e8a378dcb73
Dewmal Nilanka <dewmalnilanka@gmail.com>
2021-01-09 13:42:50 +0530

    init

---
+++ src/main/java/lk/ijse/ws/instaclone/InstacloneApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.ws.instaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstacloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstacloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/ws/instaclone/InstacloneApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.ws.instaclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstacloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
