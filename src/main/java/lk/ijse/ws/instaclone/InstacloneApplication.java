123a2d462d4ac5b8d9961a9fa7d9d351d21e2bee
Dewmal Nilanka <dewmalnilanka@gmail.com>
2021-01-16 00:10:40 +0530

    Login with Oauth2

--- src/main/java/lk/ijse/ws/instaclone/InstacloneApplication.java
+++ src/main/java/lk/ijse/ws/instaclone/InstacloneApplication.java
@@ -1,13 +1,46 @@
 package lk.ijse.ws.instaclone;
 
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.RemoteJWKSet;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.util.DefaultResourceRetriever;
+import com.nimbusds.jose.util.ResourceRetriever;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import lk.ijse.ws.instaclone.jwt.JwtConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.nimbusds.jose.JWSAlgorithm.RS256;
 
 @SpringBootApplication
 public class InstacloneApplication {
 
+    @Autowired
+    private JwtConfiguration jwtConfiguration;
+
     public static void main(String[] args) {
         SpringApplication.run(InstacloneApplication.class, args);
+    }
+
+
+    @Bean
+    public ConfigurableJWTProcessor configurableJWTProcessor() throws MalformedURLException {
+        ResourceRetriever resourceRetriever =
+                new DefaultResourceRetriever(jwtConfiguration.getConnectionTimeout(),
+                        jwtConfiguration.getReadTimeout());
+        URL jwkSetURL = new URL(jwtConfiguration.getJwkUrl());
+        JWKSource keySource = new RemoteJWKSet(jwkSetURL, resourceRetriever);
+        ConfigurableJWTProcessor jwtProcessor = new DefaultJWTProcessor();
+        JWSKeySelector keySelector = new JWSVerificationKeySelector(RS256, keySource);
+        jwtProcessor.setJWSKeySelector(keySelector);
+        return jwtProcessor;
     }
 
 }
--- src/main/java/lk/ijse/ws/instaclone/WebSecurityConfigure.java
+++ src/main/java/lk/ijse/ws/instaclone/WebSecurityConfigure.java
@@ -1,23 +1,30 @@
 package lk.ijse.ws.instaclone;
 
+import lk.ijse.ws.instaclone.jwt.AwsCognitoJwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
+
+    @Autowired
+    private AwsCognitoJwtAuthFilter awsCognitoJwtAuthenticationFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
+
+        http.headers().cacheControl();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("**/health").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .antMatchers("/**").authenticated()
                 .and()
-                .authorizeRequests(authz -> authz.mvcMatchers("/")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated())
-                .oauth2Login()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/");
+                .addFilterBefore(awsCognitoJwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
+
 }
