edcfdbd1676fe453927261642cd13661c06f01f0
Dewmal Nilanka <dewmalnilanka@gmail.com>
2021-01-15 23:32:42 +0530

    Basic Cognito

---
+++ src/main/java/lk/ijse/ws/instaclone/CognitoWebConfiguration.java
@@ -0,0 +1,15 @@
+package lk.ijse.ws.instaclone;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CognitoWebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
--- src/main/java/lk/ijse/ws/instaclone/WebSecurityConfigure.java
+++ src/main/java/lk/ijse/ws/instaclone/WebSecurityConfigure.java
@@ -1,54 +1,23 @@
 package lk.ijse.ws.instaclone;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import javax.servlet.*;
-import java.io.IOException;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        System.out.println("Security Configure");
-        System.out.println(http);
-        http
-
-                .authorizeRequests()
-//
-                .antMatchers("/**").permitAll().anyRequest().authenticated()
-                .and().csrf().disable();
-
-//                .anyRequest().permitAll();
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-    }
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+        http.csrf()
+                .and()
+                .authorizeRequests(authz -> authz.mvcMatchers("/")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .oauth2Login()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
     }
 }
